8bffb311c998e33285eda65b523d6310604da978
Niall Paterson <npaters@tcd.ie>
2016-11-24 23:40:49 +0000

    Create Store.go

--- authenticationservice/main.go
+++ authenticationservice/main.go
@@ -4,31 +4,40 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+var store Store
+
 func main() {
     r := gin.Default()
     router.POST("/signup", signup)
     router.POST("/login", login)
     router.Run(":3001")
+    store := DefaultStore
 }
 
 // Take in username and password, and store in db as (username, encrypt(password))
 func signup(ctx *gin.Context) {
-    // Create user from params (add sessionKey to db?)
-    // If successful, create request with user and Context and send response
-    token := GenerateToken()
-    response := AuthenticatedResponse{token: token}
-    encryptedResponse := response.encrypt(user.Password)
-    ctx.JSON(code, encryptedResponse.EncodeJSON())
+    err := store.CreateUser(ctx.Query("username", ctx.Query("password"))
+    if err != nil {
+        token := GenerateToken()
+        response := AuthenticatedResponse{token: token}
+        encryptedResponse := response.encrypt(user.Password)
+        ctx.JSON(code, encryptedResponse.EncodeJSON())
+    } else {
+        // Fail
+    }
 }
 
 // Find row with username in db and ensure encrypt(password) = encrypted_password
 func login(ctx *gin.Context) {
-    // Find user from params
-    // If successful, create request with user and Context and send response
-    token := GenerateToken()
-    response := AuthenticatedResponse{token: token}
-    encryptedResponse := response.encrypt(user.Password)
-    ctx.JSON(code, encryptedResponse.EncodeJSON())
+    exists := store.UserExists(ctx.Query("username", ctx.Query("password"))
+    if exists {
+        token := GenerateToken()
+        response := AuthenticatedResponse{token: token}
+        encryptedResponse := response.encrypt(user.Password)
+        ctx.JSON(code, encryptedResponse.EncodeJSON())
+    } else {
+        // Fail
+    }
 }
 
 func sampleMethod(ctx *gin.Context) {
